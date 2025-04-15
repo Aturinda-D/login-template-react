3ac2b3ec7577ed69e1566c23f393dfaf127bc942
deidave <atudav15@gmail.com>
2025-04-15 14:00:55 +0300

    Final touches

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import CompanyDetails from "./components/CompanyDetails";
 import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
+  /**
+   * === FEATURES ===
+   *  -> Responsive UI
+   *  -> Backdrop gradient
+   *  -> Interactive buttons
+   *  -> Default values for inputs
+   *  -> Company icon and name in browser tab
+   *  -> Show/hide password toggle
+   */
   return (
     <main>
       <CompanyDetails />
